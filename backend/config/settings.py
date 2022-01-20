54266d475de7c524d35eac9630ee3f284638f146
Sylvain Sainnier <sainnier@gmail.com>
2022-01-20 23:23:35 +0100

    Athentication/Permissions setup - settings.py omitted at previous commit

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver"]
+# DEFAULT_C
 
 
 # Application definition
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.gis",
     "django_filters",
+    "djoser",
     "commons.apps.BaseModelConfig",
     "cables.apps.CablesConfig",
     "mortality.apps.CaseConfig",
@@ -147,7 +149,22 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Default filter backend
 # https://www.django-rest-framework.org/api-guide/filtering/
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend"
-    ]
+    ],
+}
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("JWT",),
+}
+
+DJOSER = {
+    "PASSWORD_RESET_CONFIRM_URL": "#/password/reset/confirm/{uid}/{token}",
+    "USERNAME_RESET_CONFIRM_URL": "#/username/reset/confirm/{uid}/{token}",
+    "ACTIVATION_URL": "#/activate/{uid}/{token}",
+    "SEND_ACTIVATION_EMAIL": True,
+    "SERIALIZERS": {},
 }
