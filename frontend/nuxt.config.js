e6a20dfa9055808cac0d26be8c8a852b3683b728
Sylvain Sainnier <sainnier@gmail.com>
2022-02-03 12:04:46 +0100

    fix: Frontend: Nuxt re-installed due to bugs

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -40,16 +40,12 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    // https://auth.nuxtjs.org
+    '@nuxtjs/auth-next',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
-    // https://auth.nuxtjs.org/
-      '@nuxtjs/auth-next',
-    // https://www.npmjs.com/package/nuxt-leaflet
-      'nuxt-leaflet'
-    ],
 
-  middleware: "auth",
-
+  ],
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
