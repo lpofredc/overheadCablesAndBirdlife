5fe59d773ba070c65920d6ef3917b10730bd0a32
Sylvain Sainnier <sainnier@gmail.com>
2022-02-03 15:12:54 +0100

    feat: Backend: add CORS manager module (django-cors-headers)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver"]
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -58,6 +59,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -102,6 +104,11 @@ DATABASES = {
         "PORT": config("DBPORT", default="5432", cast=int),
     }
 }
+
+# Authorized request origins
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 
 # Password validation
