b0bc77680ddab2f904c36948afe22abe18a1c984
hypsug0 <fcloitre@dbchiro.org>
2022-03-17 21:43:17 +0100

    fix: serve uploaded images

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -30,4 +32,4 @@ urlpatterns = [
     path("api/v1/custom-content/", include("custom_content.urls")),
     path("api/v1/nomenclature/", include("sinp_nomenclatures.urls")),
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
