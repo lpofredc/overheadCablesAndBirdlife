c45dbb0ea9f10484342086048ef55eae727e8eea
hypsug0 <fcloitre@dbchiro.org>
2022-03-17 01:19:04 +0100

    feat: Partners from API

--- frontend/components/welcomeComponent.vue
+++ frontend/components/welcomeComponent.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-    <v-card>Texte d'acceil et logo </v-card>
+   <v-row dense>
+          <v-col
+      v-for="i in partners"
+      :key="i.id"
+      :loading="loading"
+            cols="12"
+            sm="12"
+            md="6"
+            lg="4"
+            class="d-flex center"
+          >
+            <v-card
+
+      class="mx-auto my-12"
+    >
+      <v-img height="100" src="https://via.placeholder.com/300.png/09f/fff"></v-img>
+
+      <v-card-title><a :href="i.url" target="_blank">{{ i.name }}</a></v-card-title>
+
+    </v-card>
+          </v-col>
+        </v-row>
+    
   </div>
 </template>
 
@@ -8,5 +29,19 @@
 export default {
   name: 'WelcomeComponent',
   auth: false,
+  data() {
+    return {
+      partners: [],
+    }
+  },
+  mounted() {
+    this.getPartners()
+  },
+  methods: {
+    async getPartners() {
+      const partners = await this.$axios.$get('custom-content/partners/')
+      this.partners = partners
+    },
+  },
 }
 </script>
