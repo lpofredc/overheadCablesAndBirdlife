98427b654f439fe52908bfb253a2faf5ca269f08
Fred C <frederic.cloitre@lpo.fr>
2022-03-21 23:55:09 +0100

    feat: update frontend meta

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -8,8 +8,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - frontend',
-    title: 'frontend',
+    titleTemplate: '%s - OCB',
+    title: 'Overhead Cables & BirdLife',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
