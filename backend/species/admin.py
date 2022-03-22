5f9f15f874ea7bbc0d35d09a485ed9741a447d82
hypsug0 <fcloitre@dbchiro.org>
2022-03-22 23:30:28 +0100

    feat: improve species admin

--- backend/species/admin.py
+++ backend/species/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Species
 
-admin.site.register(Species)
+
+class SpeciesAdmin(admin.ModelAdmin):
+    list_display = ("code", "scientific_name", "vernacular_name", "active")
+    list_filter = ("active",)
+    ordering = ("scientific_name",)
+
+
+admin.site.register(Species, SpeciesAdmin)
