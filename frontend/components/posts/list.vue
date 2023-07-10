c5f8e05f29d1c37d557183ff79b00b7c11488bbe
Fred C <42280769+hypsug0@users.noreply.github.com>
2023-07-10 15:09:54 +0200

    Display posts as timeline

--- frontend/components/posts/list.vue
+++ frontend/components/posts/list.vue
@@ -1,21 +1,37 @@
 <template>
   <v-sheet class="fill-height" dark align-self="start">
 
-    <v-list class="pa-0 bg-surface-variant overflow-y-auto fill-height" v-if="posts && posts.length > 0" lines="three">
-      <v-list-item v-for="post in posts" :key="post.id" :to="`/posts/${post.id}`"
-        prepend-avatar="https://cdn.vuetifyjs.com/images/arthur.png">
-        <v-list-item-title>
-          {{ post.title }}
-        </v-list-item-title>
-        <v-list-item-subtitle>
-          <i>{{new Date(post.timestamp_create).toISOString().substr(0,10)}}</i> - {{ post.intro }}
-        </v-list-item-subtitle>
-      </v-list-item>
-    </v-list>
+    <v-timeline side="end" align="start">
+      <v-timeline-item :dot-color="post.private ? 'pink':'green'" size="small" v-for="post in posts" :key="post.id"
+        :to="`/posts/${post.id}`">
+        <template v-slot:icon>
+          <v-icon size="x-small">{{post.private ? 'mdi-lock' : ''}}</v-icon>
+        </template>
+        <div class="d-flex">
+
+          <div @click="router.push(`/posts/${post.id}`)" class="pointer">
+            <strong><small>{{new Date(post.timestamp_create).toISOString().substr(0,10)}}</small> {{ post.title
+              }}</strong>
+            <div class="text-caption">
+              {{ post.intro }}
+            </div>
+          </div>
+        </div>
+      </v-timeline-item>
+
+    </v-timeline>
   </v-sheet>
 </template>
 
-<script setup>
+<script setup lang="ts">
+const  router = useRouter()
+
 const { data: posts } = await useHttp('/api/v1/custom-content/news/')
-console.log('POSTS', posts.value[0])
 </script>
+
+
+<style>
+.pointer {
+  cursor: pointer;
+}
+</style>
